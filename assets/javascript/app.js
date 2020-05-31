9485dcbff801ed3a07d096cf1c3034dd1e44d685
Curtis Lane <clanelmt@gmail.com>
2020-05-31 16:53:32 -0400

    add linkdin and github links to contact page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,5 @@
 $(document).ready(function(){
 
-    var iOS = navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform); // Detect if user is on Apple ios
-
-    if (iOS) {
-        $('.parallax-one', '.parallax-two', '.parallax-three').css('background-attachment', 'scroll !important')
-        $('.parallax-one', '.parallax-two', '.parallax-three').css('background-size', 'inherit !important')
-    }
-
     // Smooth scrolling effect copied from https://css-tricks.com/snippets/jquery/smooth-scrolling/
     
     // Select all links with hashes
@@ -20,30 +13,34 @@ $(document).ready(function(){
         location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
         && 
         location.hostname == this.hostname
-        ) {
-        // Figure out element to scroll to
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        // Does a scroll target exist?
-        if (target.length) {
-            // Only prevent default if animation is actually gonna happen
-            event.preventDefault();
-            $('html, body').animate({
-            scrollTop: target.offset().top
-            }, 1000, function() {
-            // Callback after animation
-            // Must change focus!
-            var $target = $(target);
-            $target.focus();
-            if ($target.is(":focus")) { // Checking if the target was focused
-                return false;
-            } else {
-                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-                $target.focus(); // Set focus again
-            };
-            });
+        ){
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, function() {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+
+                });
+
+            }
+
         }
-        }
-    });
+
+    }); // *Close .click
 
 }); // *Close document.ready
